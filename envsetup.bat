5cce56feda55189af0b8baba02a908e205123128
Evan Driscoll <driscoll@cs.wisc.edu>
2012-05-01 13:30:27 -0500

    Fix Windows envsetup script

--- envsetup.bat
+++ envsetup.bat
@@ -1,1 +1,2 @@
-set PYTHONPATH="%PYTHONPATH%:%CD%/third-party/lib/python2.7/site-packages"
+set PYTHONPATH=%PYTHONPATH%;%CD%/third-party/lib/python2.7/site-packages
+set PATH=%PATH%;C:\Program Files (x86)\Graphviz 2.28\bin;c:\python27
